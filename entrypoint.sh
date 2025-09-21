81eabcd89f73248bc81dea68afd746b841b7eeae
Onojeta Brown <onosbrown.saved@gmail.com>
2025-09-21 21:27:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+if [ -z "$HOST_SSH_PORT" ]; then
+  echo "ERROR: HOST_SSH_PORT is not set!"
+  exit 1
+fi
+
 # Substitute HOST_SSH_PORT in nginx template
 envsubst '$HOST_SSH_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Show final config (for debugging)
+cat /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
