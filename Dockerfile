206cc657790075289055307b1df9d5d17e6aa541
Onojeta Brown <onosbrown.saved@gmail.com>
2025-09-23 16:53:38 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM nginx:alpine
 
-# Install envsubst to substitute environment variables
+# Install envsubst to replace environment variables
 RUN apk add --no-cache bash gettext
 
 # Copy Nginx template
-# COPY nginx.conf.template /etc/nginx/conf.d/default.conf.template
+COPY nginx.conf.template /etc/nginx/conf.d/default.conf.template
 
 # Entrypoint script
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ if [ -z "$VPS_TUNNEL_PORT" ]; then
   exit 1
 fi
 
-# Substitute env vars in nginx template
-# envsubst '$VPS_TUNNEL_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Replace environment variable in Nginx template
+envsubst '$VPS_TUNNEL_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# echo ">>> Final nginx.conf:"
-# cat /etc/nginx/conf.d/default.conf
+echo ">>> Nginx configuration:"
+cat /etc/nginx/conf.d/default.conf
 
-# Start nginx
+# Start Nginx
 nginx -g 'daemon off;'
